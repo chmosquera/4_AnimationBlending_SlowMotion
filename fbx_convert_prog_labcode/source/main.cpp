f7e65cb5dbf4c739cb98c7690874022bd4220348
Chanelle <ckaith97@yahoo.com>
2018-05-29 14:00:40 -0700

    first commit

---
+++ fbx_convert_prog_labcode/source/main.cpp
@@ -0,0 +1,439 @@
+/*
+CPE/CSC 474 Lab base code Eckhardt/Dahl
+based on CPE/CSC 471 Lab base code Wood/Dunn/Eckhardt
+*/
+
+#include <iostream>
+#include <glad/glad.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "GLSL.h"
+#include "Program.h"
+#include "MatrixStack.h"
+
+#include "WindowManager.h"
+#include "Shape.h"
+#include "Line.h"
+#include "bone.h"
+using namespace std;
+using namespace glm;
+shared_ptr<Shape> shape;
+shared_ptr<Shape> plane;
+
+
+double get_last_elapsed_time()
+{
+	static double lasttime = glfwGetTime();
+	double actualtime =glfwGetTime();
+	double difference = actualtime- lasttime;
+	lasttime = actualtime;
+	return difference;
+}
+class camera
+{
+public:
+	glm::vec3 pos, rot;
+	int w, a, s, d;
+	camera()
+	{
+		w = a = s = d = 0;
+		pos = rot = glm::vec3(0, 0, 0);
+	}
+	glm::mat4 process(double ftime)
+	{
+		float speed = 0;
+		if (w == 1)
+		{
+			speed = 10*ftime;
+		}
+		else if (s == 1)
+		{
+			speed = -10*ftime;
+		}
+		float yangle=0;
+		if (a == 1)
+			yangle = -3*ftime;
+		else if(d==1)
+			yangle = 3*ftime;
+		rot.y += yangle;
+		glm::mat4 R = glm::rotate(glm::mat4(1), rot.y, glm::vec3(0, 1, 0));
+		glm::vec4 dir = glm::vec4(0, 0, speed,1);
+		dir = dir*R;
+		pos += glm::vec3(dir.x, dir.y, dir.z);
+		glm::mat4 T = glm::translate(glm::mat4(1), pos);
+		return R*T;
+	}
+};
+camera mycam;
+
+/*
+class calc_matrix {
+
+	float fframe;
+	int frameA = fframe;
+	int frameB = fframe + 1;
+	float t = fframe - (int)fframe;
+	quat qa = boneanim[0]->keyframes[frameA].quat;
+	quat qb = boneanim[0]->keyframes[frameB].quat;
+	quat qr = linintp(qa, qb, t);
+	mat4 R = mat4(qr);
+	vec
+} */
+
+class Application : public EventCallbacks
+{
+
+public:
+
+	WindowManager * windowManager = nullptr;
+
+	// Our shader program
+	std::shared_ptr<Program> prog, psky, pplane;
+
+	// Contains vertex information for OpenGL
+	GLuint VertexArrayID;
+
+	// Data necessary to give our box to OpenGL
+	GLuint VertexBufferID, VertexBufferIDimat, VertexNormDBox, VertexTexBox, IndexBufferIDBox;
+
+	//texture data
+	GLuint Texture;
+	GLuint Texture2;
+
+	//animation matrices:
+	mat4 animmat[200];
+	int animmatsize=0;
+
+	void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+		
+		if (key == GLFW_KEY_W && action == GLFW_PRESS)
+		{
+			mycam.w = 1;
+		}
+		if (key == GLFW_KEY_W && action == GLFW_RELEASE)
+		{
+			mycam.w = 0;
+		}
+		if (key == GLFW_KEY_S && action == GLFW_PRESS)
+		{
+			mycam.s = 1;
+		}
+		if (key == GLFW_KEY_S && action == GLFW_RELEASE)
+		{
+			mycam.s = 0;
+		}
+		if (key == GLFW_KEY_A && action == GLFW_PRESS)
+		{
+			mycam.a = 1;
+		}
+		if (key == GLFW_KEY_A && action == GLFW_RELEASE)
+		{
+			mycam.a = 0;
+		}
+		if (key == GLFW_KEY_D && action == GLFW_PRESS)
+		{
+			mycam.d = 1;
+		}
+		if (key == GLFW_KEY_D && action == GLFW_RELEASE)
+		{
+			mycam.d = 0;
+		}
+	}
+
+	// callback for the mouse when clicked move the triangle when helper functions
+	// written
+	void mouseCallback(GLFWwindow *window, int button, int action, int mods)
+	{
+	}
+
+	//if the window is resized, capture the new size and reset the viewport
+	void resizeCallback(GLFWwindow *window, int in_width, int in_height)
+	{
+		//get the window size - may be different then pixels for retina
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+	}
+
+	/*Note that any gl calls must always happen after a GL state is initialized */
+	bone *root = NULL;
+	int size_stick = 0;
+	all_animations all_animation;
+	void initGeom(const std::string& resourceDirectory)
+	{
+
+		for (int ii = 0; ii < 200; ii++)
+			animmat[ii] = mat4(1);
+		
+		readtobone("LeBron.fbx",&all_animation,&root);
+		root->set_animations(&all_animation,animmat,animmatsize);
+		
+			
+		// Initialize mesh.
+		shape = make_shared<Shape>();
+		shape->loadMesh(resourceDirectory + "/skybox.obj");
+		shape->resize();
+		shape->init();
+
+		plane = make_shared<Shape>();
+		plane->loadMesh(resourceDirectory + "/FA18.obj");
+		plane->resize();
+		plane->init();
+
+		//generate the VAO
+		glGenVertexArrays(1, &VertexArrayID);
+		glBindVertexArray(VertexArrayID);
+
+		//generate vertex buffer to hand off to OGL
+		glGenBuffers(1, &VertexBufferID);
+		//set the current state to focus on our vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+		
+		vector<vec3> pos;
+		vector<unsigned int> imat;
+		root->write_to_VBOs(vec3(0, 0, 0), pos, imat);
+		size_stick = pos.size();
+		//actually memcopy the data - only do this once
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vec3)*pos.size(), pos.data(), GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		//indices of matrix:
+		glGenBuffers(1, &VertexBufferIDimat);
+		glBindBuffer(GL_ARRAY_BUFFER, VertexBufferIDimat);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uint)*imat.size(), imat.data(), GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(1);
+		glVertexAttribIPointer(1, 1, GL_UNSIGNED_INT, 0, (void*)0);
+
+		glBindVertexArray(0);
+
+
+		/************ Load Textures **********/
+		int width, height, channels;
+		char filepath[1000];
+
+		//texture 1
+		string str = resourceDirectory + "/explosion.jpg";
+		strcpy(filepath, str.c_str());
+		unsigned char* data = stbi_load(filepath, &width, &height, &channels, 4);
+		glGenTextures(1, &Texture);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		//texture 2
+		str = resourceDirectory + "/grid.jpg";
+		strcpy(filepath, str.c_str());
+		data = stbi_load(filepath, &width, &height, &channels, 4);
+		glGenTextures(1, &Texture2);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, Texture2);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		//[TWOTEXTURES]
+		//set the 2 textures to the correct samplers in the fragment shader:
+		GLuint Tex1Location = glGetUniformLocation(prog->pid, "tex");//tex, tex2... sampler in the fragment shader
+		GLuint Tex2Location = glGetUniformLocation(prog->pid, "tex2");
+		// Then bind the uniform samplers to texture units:
+		glUseProgram(prog->pid);
+		glUniform1i(Tex1Location, 0);
+		glUniform1i(Tex2Location, 1);
+
+	}
+
+	//General OGL initialization - set OGL state here
+	void init(const std::string& resourceDirectory)
+	{
+		GLSL::checkVersion();
+
+		// Set background color.
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		// Enable z-buffer test.
+		glEnable(GL_DEPTH_TEST);
+		//glDisable(GL_DEPTH_TEST);
+		// Initialize the GLSL program.
+		prog = std::make_shared<Program>();
+		prog->setVerbose(true);
+		prog->setShaderNames(resourceDirectory + "/shader_vertex.glsl", resourceDirectory + "/shader_fragment.glsl");
+		if (!prog->init())
+		{
+			std::cerr << "One or more shaders failed to compile... exiting!" << std::endl;
+			exit(1);
+		}
+		prog->addUniform("P");
+		prog->addUniform("V");
+		prog->addUniform("M");
+		prog->addUniform("Manim");
+		prog->addUniform("campos");
+		prog->addAttribute("vertPos");
+		prog->addAttribute("vertimat");
+
+
+		psky = std::make_shared<Program>();
+		psky->setVerbose(true);
+		psky->setShaderNames(resourceDirectory + "/skyvertex.glsl", resourceDirectory + "/skyfrag.glsl");
+		if (!psky->init())
+		{
+			std::cerr << "One or more shaders failed to compile... exiting!" << std::endl;
+			exit(1);
+		}
+		psky->addUniform("P");
+		psky->addUniform("V");
+		psky->addUniform("M");
+		psky->addUniform("campos");
+		psky->addAttribute("vertPos");
+		psky->addAttribute("vertNor");
+		psky->addAttribute("vertTex");
+	}
+
+
+	/****DRAW
+	This is the most important function in your program - this is where you
+	will actually issue the commands to draw any geometry you have set up to
+	draw
+	********/
+
+	
+	void render()
+	{
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		double frametime = get_last_elapsed_time();
+		static double totaltime_ms=0;
+		totaltime_ms += frametime*1000.0;
+		static double totaltime_untilframe_ms = 0;
+		totaltime_untilframe_ms += frametime*1000.0;
+
+		for (int ii = 0; ii < 200; ii++)
+			animmat[ii] = mat4(1);
+
+
+		//animation frame system
+		int anim_step_width_ms = 8490 / 204;
+		static int frame = 0;
+		if (totaltime_untilframe_ms >= anim_step_width_ms)
+			{
+			totaltime_untilframe_ms = 0;
+			frame++;
+			}
+		root->play_animation(frame,"avatar_0_fbx_tmp");	//name of current animation	
+
+
+		// Get current frame buffer size.
+		int width, height;
+		glfwGetFramebufferSize(windowManager->getHandle(), &width, &height);
+		float aspect = width/(float)height;
+		glViewport(0, 0, width, height);
+
+		// Clear framebuffer.
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Create the matrix stacks - please leave these alone for now
+		
+		glm::mat4 V, M, P; //View, Model and Perspective matrix
+		V = mycam.process(frametime);
+		M = glm::mat4(1);
+		P = glm::perspective((float)(3.14159 / 4.), (float)((float)width/ (float)height), 0.1f, 1000.0f); //so much type casting... GLM metods are quite funny ones
+		float sangle = 3.1415926 / 2.;
+		glm::vec3 camp = -mycam.pos;
+		glm::mat4 TransSky = glm::translate(glm::mat4(1.0f), camp);
+		glm::mat4 SSky = glm::scale(glm::mat4(1.0f), glm::vec3(0.8f, 0.8f, 0.8f));
+
+		M = TransSky * SSky;
+
+		// Draw the sky using GLSL.
+		psky->bind();		
+		glUniformMatrix4fv(psky->getUniform("P"), 1, GL_FALSE, &P[0][0]);
+		glUniformMatrix4fv(psky->getUniform("V"), 1, GL_FALSE, &V[0][0]);
+		glUniformMatrix4fv(psky->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glUniform3fv(psky->getUniform("campos"), 1, &mycam.pos[0]);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture2);
+		glDisable(GL_DEPTH_TEST);
+		shape->draw(psky, false);			//render!!!!!!!
+		glEnable(GL_DEPTH_TEST);	
+		psky->unbind();
+
+		//draw the lines
+		prog->bind();
+		//send the matrices to the shaders
+		glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, &P[0][0]);
+		glUniformMatrix4fv(prog->getUniform("V"), 1, GL_FALSE, &V[0][0]);
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glUniform3fv(prog->getUniform("campos"), 1, &mycam.pos[0]);	
+		glBindVertexArray(VertexArrayID);
+		//actually draw from vertex 0, 3 vertices
+		//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, (void*)0);
+		mat4 Vi = glm::transpose(V);
+		Vi[0][3] = 0;
+		Vi[1][3] = 0;
+		Vi[2][3] = 0;
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+
+		glm::mat4 TransZ = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -8));
+		glm::mat4 S = glm::scale(glm::mat4(1.0f), glm::vec3(0.01f, 0.01f, 0.01f));
+		M = TransZ * S;
+		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
+		glUniformMatrix4fv(prog->getUniform("Manim"), 200, GL_FALSE, &animmat[0][0][0]);
+		glDrawArrays(GL_LINES, 4, size_stick-4);
+		glBindVertexArray(0);		
+		prog->unbind();
+
+
+	}
+
+};
+//******************************************************************************************
+int main(int argc, char **argv)
+{
+	std::string resourceDir = "../resources"; // Where the resources are loaded from
+	if (argc >= 2)
+	{
+		resourceDir = argv[1];
+	}
+
+	Application *application = new Application();
+
+	/* your main will always include a similar set up to establish your window
+		and GL context, etc. */
+	WindowManager * windowManager = new WindowManager();
+	windowManager->init(1920, 1080);
+	windowManager->setEventCallbacks(application);
+	application->windowManager = windowManager;
+
+	/* This is the code that will likely change program to program as you
+		may need to initialize or set up different data and state */
+	// Initialize scene.
+	application->init(resourceDir);
+	application->initGeom(resourceDir);
+
+	// Loop until the user closes the window.
+	while(! glfwWindowShouldClose(windowManager->getHandle()))
+	{
+		// Render scene.
+		application->render();
+
+		// Swap front and back buffers.
+		glfwSwapBuffers(windowManager->getHandle());
+		// Poll for and process events.
+		glfwPollEvents();
+	}
+
+	// Quit program.
+	windowManager->shutdown();
+	return 0;
+}
